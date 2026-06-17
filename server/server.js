5e3da7af30e0f3c126698b0e5108654f77e57b45
Gayathri <gayathribharathi895@gmail.com>
2026-06-17 16:44:35 +0530

    backend update

--- server/server.js
+++ server/server.js
@@ -2,23 +2,29 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
-dotenv.config({ path: "./.env" });
+dotenv.config();
 
 const connectDB = require("./config/db");
 const leadRoutes = require("./routes/leadRoutes");
 
+// Connect MongoDB
 connectDB();
 
 const app = express();
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
+
+// Routes
 app.use("/api/leads", leadRoutes);
 
+// Test route
 app.get("/", (req, res) => {
   res.send("Mini CRM API Running");
 });
 
+// PORT for deployment (VERY IMPORTANT)
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
